cdea0838ebd708b58b72217b52bac8781c7b57a1
Skowoo <skowoo@tlen.pl>
2023-08-10 10:08:02 +0200

    Part 1 done (creation of framework and Student related data models)

---
+++ TutorialWebApp2/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TutorialWebApp2.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+
+        public string Title { get; set; }
+
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
--- TutorialWebApp2/Program.cs
+++ TutorialWebApp2/Program.cs
@@ -1,3 +1,7 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TutorialWebApp2.Data;
+
 namespace TutorialWebApp2
 {
     public class Program
@@ -5,6 +9,8 @@ namespace TutorialWebApp2
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<SchoolContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SchoolContext") ?? throw new InvalidOperationException("Connection string 'SchoolContext' not found.")));
 
             // Add services to the container.
             builder.Services.AddRazorPages();
@@ -17,6 +23,15 @@ namespace TutorialWebApp2
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+            }
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var context = services.GetRequiredService<SchoolContext>();
+                context.Database.EnsureCreated();
+                DbInitializer.Initialize(context);
             }
 
             app.UseHttpsRedirection();
