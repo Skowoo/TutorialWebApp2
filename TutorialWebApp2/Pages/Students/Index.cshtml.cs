71651052f9a52626458679f3d22a634c69237319
Skowoo <skowoo@tlen.pl>
2023-08-10 13:13:16 +0200

    Part 3 in progress. TBC on Grouping

--- TutorialWebApp2/Pages/Students/Index.cshtml.cs
+++ TutorialWebApp2/Pages/Students/Index.cshtml.cs
@@ -1,32 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using TutorialWebApp2.Data;
 using TutorialWebApp2.Models;
+using TutorialWebApp2.Data;
+using Microsoft.Extensions.Configuration;
 
 namespace TutorialWebApp2.Pages.Students
 {
     public class IndexModel : PageModel
     {
-        private readonly TutorialWebApp2.Data.SchoolContext _context;
+        private readonly SchoolContext _context;
 
-        public IndexModel(TutorialWebApp2.Data.SchoolContext context)
+        private readonly IConfiguration Configuration;
+
+        public IndexModel(SchoolContext context, IConfiguration configuration)
         {
             _context = context;
+            Configuration = configuration;
         }
 
-        public IList<Student> Student { get;set; } = default!;
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        public PaginatedList<Student> Students { get; set; }
+
+        public async Task OnGetAsync(string sortOrder,
+            string currentFilter, string searchString, int? pageIndex)
         {
-            if (_context.Students != null)
+            CurrentSort = sortOrder;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
             {
-                Student = await _context.Students.ToListAsync();
+                pageIndex = 1;
             }
+            else
+            {
+                searchString = currentFilter;
+            }
+            CurrentFilter = searchString;
+
+            IQueryable<Student> studentsIQ = from s in _context.Students
+                                             select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                studentsIQ = studentsIQ.Where(s => s.LastName.Contains(searchString) || 
+                                                   s.FirstMidName.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    studentsIQ = studentsIQ.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            var pageSize = Configuration.GetValue("PageSize", 4);
+
+            Students = await PaginatedList<Student>.CreateAsync(studentsIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
+            //null-coalescing operator defines a default value for a nullable type.
+            //The expression pageIndex ?? 1 returns the value of pageIndex if it has a value, otherwise, it returns 1.
         }
+        #region original version
+        //public async Task OnGetAsync()
+        //{
+        //    if (_context.Students != null)
+        //    {
+        //        Students = await _context.Students.ToListAsync();
+        //    }
+        //}
+        #endregion
     }
 }
