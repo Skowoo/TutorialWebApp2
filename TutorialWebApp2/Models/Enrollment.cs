7c74e0e771db4dacde58733dbb10be60915f9e09
Skowoo <skowoo@tlen.pl>
2023-08-11 09:28:44 +0200

    Part 5 done (creation of new model for enlarged database)

--- TutorialWebApp2/Models/Enrollment.cs
+++ TutorialWebApp2/Models/Enrollment.cs
@@ -11,15 +11,16 @@ namespace TutorialWebApp2.Models
     {
         public int EnrollmentID { get; set; }
 
+
         public int CourseID { get; set; }
+        public Course Course { get; set; }
+
 
         public int StudentID { get; set; }
+        public Student Student { get; set; }
+
 
         [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
-
-        public Course Course { get; set; }
-
-        public Student Student { get; set; }
     }
 }
