f1c69cd89aac3767a6374374c58da09df881b7f9
Skowoo <skowoo@tlen.pl>
2023-08-11 13:18:13 +0200

    Part 7 in progress. TBD: Update the Course page models

--- TutorialWebApp2/Pages/Courses/Create.cshtml.cs
+++ TutorialWebApp2/Pages/Courses/Create.cshtml.cs
@@ -10,7 +10,8 @@ using TutorialWebApp2.Models;
 
 namespace TutorialWebApp2.Pages.Courses
 {
-    public class CreateModel : PageModel
+    public class CreateModel : DepartmentNamePageModel 
+                                //Derive from PageModel with SelectList instead of PageModel
     {
         private readonly TutorialWebApp2.Data.SchoolContext _context;
 
@@ -21,7 +22,7 @@ namespace TutorialWebApp2.Pages.Courses
 
         public IActionResult OnGet()
         {
-        ViewData["DepartmentID"] = new SelectList(_context.Departments, "DepartmentID", "DepartmentID");
+            PopulateDepartmentsDropDownList(_context);
             return Page();
         }
 
@@ -32,15 +33,18 @@ namespace TutorialWebApp2.Pages.Courses
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid)
+            var emptyCourse = new Course();
+
+            if (await TryUpdateModelAsync<Course>(emptyCourse, "course" /*Prefix for form value.*/, s => s.CourseID, s => s.DepartmentID, s => s.Title, s => s.Credits))
             {
-                return Page();
+                _context.Courses.Add(emptyCourse);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
             }
 
-            _context.Courses.Add(Course);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
+            // Select DepartmentID if TryUpdateModelAsync fails.
+            PopulateDepartmentsDropDownList(_context, emptyCourse.DepartmentID);
+            return Page();
         }
     }
 }
